5d4628371ddd83c8d842c30c0024a700927fe49b
learningnoobi <fanime492@gmail.com>
2021-06-21 00:31:41 +0545

    Login and Logout screen

---
+++ screens/Login.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { View, Text ,TextInput,StyleSheet ,Image,TouchableOpacity} from "react-native";
+import {authStyles} from '../styles/authStyle'
+
+const Register = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={authStyles.upper}>
+        <View style={{alignItems:'center'}}>
+            
+          <Text style={authStyles.maintext}>
+          Academia International College</Text>
+          <View style={{marginTop:5,height:5,width:200,backgroundColor:'#f9f9f9'}}></View>
+        </View>
+        <View style={{marginTop:50}}>
+          <Text style={authStyles.maintext}>Login</Text>
+        </View>
+      </View>
+
+      <View style={authStyles.lower}>
+          <View>
+            <TextInput 
+             placeholder="Email"
+            style={authStyles.input}/>
+             <TextInput 
+             placeholder="Password"
+            style={authStyles.input}/>
+          </View>
+          <View style={{justifyContent:'center',alignItems:'center',marginTop:40}}>
+            <TouchableOpacity style={authStyles.btn}>
+                <Text style={authStyles.text}>Login</Text>
+            </TouchableOpacity>
+            <Text style={{marginTop:10,fontSize:18}}>Already have account? 
+            <Text style={{color:'#666',fontWeight:'bold'}}>Register</Text></Text>
+          </View>
+      </View>
+    </View>
+  );
+};
+
+export default Register;
+
---
+++ styles/authStyle.js
@@ -0,0 +1,48 @@
+
+import {StyleSheet}  from "react-native";
+
+export const authStyles = StyleSheet.create({
+    upper:{
+        backgroundColor:'#4C367B',
+        padding:40,
+        height:250,
+        alignItems:'center'
+    },
+    maintext:{
+        color:"white",
+        fontSize:33,
+        textAlign:'center'
+    },
+    text:{
+        color:"white",
+        fontSize:18,
+    },
+    input: {
+      height: 50,
+      margin: 12,
+      padding:10,
+      fontSize:18,
+      backgroundColor:'#CDD1EF',
+      color:'#444',
+      borderRadius:3
+    },
+    lower:{
+        marginTop:-30,
+        flex:1,
+        backgroundColor:'#f5f5f5',
+        borderTopRightRadius:22,
+        borderTopLeftRadius:22,
+        padding:30,
+        
+
+    },
+    btn:{
+        height: 47,
+        justifyContent:'center',
+        alignItems:'center',
+        width:200,
+        backgroundColor:'#4F60F4',
+        borderRadius:4,
+    }
+  });
+  
